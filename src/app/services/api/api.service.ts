2ff33f1d535854625e2ebb19c492fe459fc1c1c3
Mike Godenzi <m.godenzi@papers.ch>
2021-01-11 13:04:16 +0100

    chore(): uncommented again real backend url

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 
-// const URL = 'https://tz-wrapped.dev.gke.papers.tech'
-const URL = 'http://localhost'
+const URL = 'https://tz-wrapped.dev.gke.papers.tech'
+// const URL = 'http://localhost'
 
 export enum UserState {
   ACTIVE = 'active',
