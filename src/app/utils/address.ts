8bdd427c536a4c91328681d89439cabd05fd8e8d
Mike Godenzi <m.godenzi@papers.ch>
2022-03-16 14:59:05 +0100

    fix(transfer-to-kt1): fixed address validation for transfers

--- src/app/utils/address.ts
+++ src/app/utils/address.ts
@@ -3,7 +3,10 @@ export const validateAddress = (address: string | undefined | null) => {
     address === undefined ||
     address === null ||
     address.length !== 36 ||
-    !address.startsWith('tz1')
+    (!address.startsWith('tz1') &&
+      !address.startsWith('tz2') &&
+      !address.startsWith('tz3') &&
+      !address.startsWith('KT1'))
   ) {
     throw new Error(`Invalid address: ${address}`)
   }
